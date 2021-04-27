8197ac4693633a8793174647ec00069c4c46fea1
Jaime Salgado <jsalg10@gmail.com>
2021-04-26 21:50:24 -0500

    added ingridients to js file along with html. Changed background in css style

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -11,9 +11,15 @@ function getDrink(){
         let drinkName = data.drinks[0].strDrink
         let drinkThumb = data.drinks[0].strDrinkThumb
         let drinkInst = data.drinks[0].strInstructions
+        let drinkIngredients = data.drinks[0].strIngredient1
+        let drinkIngredients2 = data.drinks[0].strIngredient2
+
         document.querySelector('h2').innerText = drinkName
         document.querySelector('img').src = drinkThumb
         document.querySelector('h3').innerText = drinkInst
+        document.querySelector('h4').innerText = drinkIngredients
+        document.querySelector('h5').innerText = drinkIngredients2
+
       })
       .catch(err => {
           console.log(`error ${err}`)
