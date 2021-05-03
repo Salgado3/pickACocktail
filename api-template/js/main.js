df42fae0227422a532b23f2c66e8e63c223b0885
Jaime Salgado <jsalg10@gmail.com>
2021-05-03 10:03:02 -0500

    updated css to put ingredient section. Updated JS to show what is needed and what amount

--- api-template/js/main.js
+++ api-template/js/main.js
@@ -9,25 +9,26 @@ function getDrink(){
       .then(data => {
         console.log(data)
         let randomDrink = Math.floor(Math.random() * 5)
+
+    
         let drinkName = data.drinks[randomDrink].strDrink
         let drinkThumb = data.drinks[randomDrink].strDrinkThumb
         let drinkInst = data.drinks[randomDrink].strInstructions
+
+        let drinkIngredients = data.drinks[randomDrink].strIngredient1 +" "+ data.drinks[randomDrink].strMeasure1
+        let drinkIngredients2 = data.drinks[randomDrink].strIngredient2 +" "+ data.drinks[randomDrink].strMeasure2
+        let drinkIngredients3 = data.drinks[randomDrink].strIngredient3 +" "+ data.drinks[randomDrink].strMeasure3
+        let drinkIngredients4 = data.drinks[randomDrink].strIngredient4 +" "+ data.drinks[randomDrink].strMeasure4
+        let drinkIngredients5 = data.drinks[randomDrink].strIngredient5 +" "+ data.drinks[randomDrink].strMeasure5
         
         document.querySelector('h2').innerText = drinkName
         document.querySelector('img').src = drinkThumb
-        document.querySelector('h3').innerText = drinkInst})
-        // document.querySelector('h4').innerText = drinkIngredients
-        // document.querySelector('h5').innerText = drinkIngredients2
+        // document.querySelector('h3').innerText = drinkInst
+        document.querySelector('h4').innerText = drinkIngredients
+        document.querySelector('h5').innerText = drinkIngredients2
 
-      // }),
-      // .then data => {
-      //   let drinkIngredients = data.drinks[0].strIngredient1
-      //   let drinkIngredients2 = data.drinks[0].strIngredient2
-      //   let drinkIngredients2 = data.drinks[0].strIngredient3
-      //   let drinkIngredients2 = data.drinks[0].strIngredient4
-      //   let drinkIngredients2 = data.drinks[0].strIngredient5
-
-      // }
+      })
+    
       .catch(err => {
           console.log(`error ${err}`)
       })
